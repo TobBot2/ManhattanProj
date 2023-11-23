7a4e8218cddd74b183fffdfc82cb7843c84129ed
TobBot2 <38795143+TobBot2@users.noreply.github.com>
2023-11-23 15:45:17 -0500

    finished basic led data structure and functions

--- main.py
+++ main.py
@@ -2,11 +2,46 @@ import board
 import neopixel
 import digitalio
 
+from dataclasses import dataclass
+from typing import List, Dict
+from enum import Enum
+
 @dataclass
-Class Led:
-    index: number
-    x: number
-    y: number
+class Led: # first three args required on init
+    x: float
+    y: float
+    index: int
+    neighbors_indeces: List[int] = []
+    powered: float = 0 # seconds until powered off
+    intensity: float = 1
+
+@dataclass
+class SortedLed:
+    led: Led
+    weight: float
+
+class Mode(Enum):
+    WHITE = 1
+    RED = 2
+    GREEN = 3
+    BLUE = 4
+    RAINBOW = 5
+    SUBWAY_COLORS = 6 # official colors from mta
+    BREATHE_WHITE = 7
+    BREATHE_RED = 8
+    BREATHE_GREEN = 9
+    BREATHE_BLUE = 10
+    WASH_DOWN_WHITE = 11
+    WASH_DOWN_RAINBOW = 12
+    SIMULATE = 13 # simulate trains going down the lines
+    SIMULATE_WHITE = 14
+
+
+class Speed(Enum):
+    NORMAL = 1
+    FAST = 2
+    SLOW = 3
+    SLOW_X = 4
 
 # NEOPIXEL SETUP
 num_pixels = 8
@@ -14,34 +49,60 @@ pixels = neopixel.NeoPixel(board.GP0, num_pixels)
 pixels.brightness = 0.2
 hue = 0
 
-# BUTTON SETUP
-button = digitalio.DigitalInOut(board.GP2)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.UP
-button_state = False
+# MODE BUTTON SETUP
+mode_btn = digitalio.DigitalInOut(board.GP2)
+mode_btn.direction = digitalio.Direction.INPUT
+mode_btn.pull = digitalio.Pull.UP
+mode_btn_state = False
+mode: Mode = Mode.WHITE
 
-"""
-LED DATA STRUCTURE
+""" *******************
+****** LED LINES ******
+******************* """
+# makes a list of leds based on first and last led in the strip
+def generate_strip(led1: Led, led2: Led) -> List[Led]:
+    count = led2.index - led1.index
+    dy = (led2.y - led1.y) / count
+    dx = (led2.x - led1.x) / count
 
-the list 'leds' is a list where the index corresponds to the led's
-index in the 'pixels' variable from the neopixel library.
-Additionally,
+    leds = []
+    for i in range(count+1): # +1 includes led2
+        if i == 0:
+            leds.append(Led(led1.x, led1.y, led1.index, neighbors_indeces=[i+1])) # starting led only has forward index
+        elif i == count:
+            leds.append(Led(led2.x, led2.y, led2.index, neighbors_indeces=[i-1])) # ending led only has previous index
+        else:
+            leds.append(Led(led1.x + i*dx, led1.y + i*dy, led1.index + i, neighbors_indeces=[i+1, i-1])) # normally they have forward & backwards index
 
-"""
+    return leds
 
-lines: List[List[number]] = [
-    # example lines for now. String of the led indexes
-    [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,51,52,53,54,55,56,57,58,59,60,61,62,233,234,235] # 123 line
-    [31,32,33,34,35,36,37,38,9,10,11,12,13,14,15,16,17,18,19,20,51,52,53,54,55,56,57,58,59,60,61,62,233,234,235] # 456 line
-]
+# manually enter the leds for the lines
+line_123 = [*generate_strip(Led(0, 0, 0), Led(100, 0, 10)), *generate_strip(Led(73, 2, 30), Led(100, 20, 35))]   # 123 line
+junction = next((x for x in line_123 if x.index == 7), None) # find led whose index is 7
+if junction is not None:
+    junction.next_index = 30
+else:
+    print("error creating junction in the 123 line")
 
-# each element in the list is a subway line containing a list of LEDs
-leds: List[Led] = [
-    [],
-]
+line_456 = []
 
-def linearize(led1: Led, led2: Led):
-    pass
+lines: Dict[str, List[Led]] = {
+    "123": line_123,
+    "456": line_456
+}
+
+def sort_by_x() -> List[SortedLed]:
+    sorted_leds: List[SortedLed] = []
+    for i in range(lines.count):
+        minimum: SortedLed = None
+        for key, leds in lines.items():
+            for led in leds:
+                # loop through all leds
+                weight = led.x
+                if minimum is None or (not led in sorted_leds and weight < minimum.weight):
+                    sorted_leds.append(SortedLed(led, weight))
+
+    return sorted_leds
 
 def hsv_to_rgb(h, s, v):
     hh = None
@@ -55,7 +116,7 @@ def hsv_to_rgb(h, s, v):
     g = None
     b = None
 
-    if s <= 0.0:  # < is bogus, just shuts up warnings
+    if s <= 0.0: # < is bogus, just shuts up warnings
         return (v * 255, v * 255, v * 255)
     hh = h
     if hh >= 360.0:
@@ -95,16 +156,20 @@ def hsv_to_rgb(h, s, v):
         b = q
     return (r * 255, g * 255, b * 255)
 
+def handle_mode_switch():
+    button_pressed = mode_btn.value
+    if button_pressed != mode_btn_state and button_pressed:
+        mode = (mode + 1) % len(Mode) # loop through mode
+        print("mode = ", mode)
+    mode_btn_state = button_pressed
+
 
 while True:
-    button_pressed = button.value
-    if button_pressed != button_state and button_pressed:
-        print("pressed")
-    button_state = button_pressed
+    handle_mode_switch()
 
     hue += 2
     for i in range(len(pixels)):
-        if button_state:
+        if mode == Mode.WHITE:
             pixels[i] = hsv_to_rgb((hue + i * 360 / len(pixels)) % 360, 0.8, 0.8)
         else:
             pixels[i] = (100, 100, 5)
