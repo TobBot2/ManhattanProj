3b3644844587a50d11641959989dcd781e3427c4
TobBot2 <38795143+TobBot2@users.noreply.github.com>
2023-12-04 20:54:10 -0500

    messed around with mtaapi.py

--- mtaapi.py
+++ mtaapi.py
@@ -1,10 +1,15 @@
 import os
 import dotenv
+import sys
 
 from nyct_gtfs import NYCTFeed
 
 dotenv.load_dotenv()
-MTA_API_KEY: str = os.getenv('MTA_API_KEY')
+MTA_API_KEY: str | None = os.getenv('MTA_API_KEY')
+
+if not MTA_API_KEY:
+    print("Error - API key not found")
+    sys.exit(1)
 
 feed: NYCTFeed = NYCTFeed("1", api_key=MTA_API_KEY)
 
